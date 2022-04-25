6113565b86eeab1e238a30b0db223df51ec6c42e
stefano corso <stefanocorso6@gmail.com>
2022-04-25 10:45:48 -0300

    respaldo

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 
-const ItemCount = (props) => {
-    const [count, setCount] =useState(props.initial)
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [count, setCount] =useState(initial)
+    const [confirmado, setConfirmado] =useState(false)
+    const [isOpen, setIsOpen] =useState(false)
+    
 
     const sumaCount = () => {
-        if (count < props.stock){
+        if (count < stock){
             setCount (count + 1)
         }
     }
@@ -14,24 +17,58 @@ const ItemCount = (props) => {
         }
     }
     const agregar = () => {
-        props.onAdd(count)
-        console.log(count)
+        onAdd(count)
+        setConfirmado(true)
+    }
+    const cancelar = () =>{
+        setCount(initial)
+        setConfirmado(false)    
+    }
+    const close = () =>{
+        setIsOpen(!isOpen)
     }
 
-  return (
-    <>
-        <div className="item__card">
-            <h2 className="titulo__producto">Producto</h2>
-            <div className="item__count">
-                <button className="item__count__button material-icons" onClick={restaCount}>remove</button>
-                <h1>{count}</h1>
-                <button className="item__count__button material-icons" onClick={sumaCount}>add</button>
-            </div>
-            <div className="item__container__button__agregar">
-                <button className="button__agregar__carrito " onClick={agregar}>Agregar al carrito</button>
-            </div>
-        </div>
-    </>
-  )
+    if(!confirmado){
+        return (
+            <>
+                <div className="item__card">
+                    <h2 className="titulo__producto">Producto</h2>
+                    <div className="item__count">
+                        <button className="item__count__button material-icons" onClick={restaCount}>remove</button>
+                        <h1>{count}</h1>
+                        <button className="item__count__button material-icons" onClick={sumaCount}>add</button>
+                    </div>
+                    <div className="item__container__button__agregar">
+                        <button className="button__agregar__carrito " onClick={agregar}>Agregar al carrito</button>
+                    </div>
+                </div>
+            </>
+          )
+
+    }else{
+        return (
+            <>
+                <div className="item__card">
+                    <h2 className="titulo__producto">Producto</h2>
+                    <div className="item__count">
+                        <button className="item__count__button material-icons" onClick={restaCount}>remove</button>
+                        <h1>{count}</h1>
+                        <button className="item__count__button material-icons" onClick={sumaCount}>add</button>
+                    </div>
+                    <div className="item__container__button__agregar">
+                        <button className="button__agregar__carrito " onClick={agregar}>Agregar al carrito</button>
+                    </div> 
+                </div>
+                <div className= {`item__card__modal ${isOpen ? 'item__card__modal__active' : '' } `} >
+                        <button className="button__close material-icons" onClick={close}>close</button>
+                        <p className="modal__parrafo" >Se confirmaron {count} unidades!</p>
+                        <button className="button__cancelar" onClick={cancelar}>Cancelar</button>
+                </div>
+            </>
+          )
+
+    }
+
+  
 }
 export default ItemCount
