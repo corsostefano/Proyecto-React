f93426e5e2fdd39f7827241985069ba22992b41c
stefano corso <stefanocorso6@gmail.com>
2022-04-26 09:27:16 -0300

    correcciones

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 
+import ModalItem from "./ModalItem"
+
 const ItemCount = ({stock, initial, onAdd}) => {
     const [count, setCount] =useState(initial)
-    const [confirmado, setConfirmado] =useState(false)
+    const [confirmed, setConfirmed] =useState(false)
     
     
 
@@ -18,17 +20,15 @@ const ItemCount = ({stock, initial, onAdd}) => {
     }
     const agregar = () => {
         onAdd(count)
-        setConfirmado(true)
+        setConfirmed(true)
     }
-    const cancelar = () =>{
+    const cancel = () =>{
         setCount(initial)
-        setConfirmado(false)    
+        setConfirmed(false)    
     }
     const close = () =>{
-        setConfirmado(false)
+        setConfirmed(false)
     }
-
-    if(!confirmado){
         return (
             <>
                 <div className="item__card">
@@ -42,33 +42,15 @@ const ItemCount = ({stock, initial, onAdd}) => {
                         <button className="button__agregar__carrito " onClick={agregar}>Agregar al carrito</button>
                     </div>
                 </div>
+                {
+                    confirmed && (
+                        <ModalItem 
+                        close = {close}
+                        count = {count}
+                        cancel = {cancel}/>
+                    )
+                }
             </>
           )
-
-    }else{
-        return (
-            <>
-                <div className="item__card">
-                    <h2 className="titulo__producto">Producto</h2>
-                    <div className="item__count">
-                        <button className="item__count__button material-icons" onClick={restaCount}>remove</button>
-                        <h1>{count}</h1>
-                        <button className="item__count__button material-icons" onClick={sumaCount}>add</button>
-                    </div>
-                    <div className="item__container__button__agregar">
-                        <button className="button__agregar__carrito " onClick={agregar}>Agregar al carrito</button>
-                    </div> 
-                </div>
-                <div className= "item__card__modal" >
-                        <button className="button__close material-icons" onClick={close}>close</button>
-                        <p className="modal__parrafo" >Se confirmaron {count} unidades!</p>
-                        <button className="button__cancelar" onClick={cancelar}>Cancelar</button>
-                </div>
-            </>
-          )
-
-    }
-
-  
 }
 export default ItemCount
